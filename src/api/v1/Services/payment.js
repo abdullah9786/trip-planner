af344f130c444bc86b79e4ffea83f7cc634d4d9d
ansari.a <79846708+abdullah9786@users.noreply.github.com>
2024-03-23 07:31:50 +0530

    changes

--- src/api/v1/Services/payment.js
+++ src/api/v1/Services/payment.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const { BadRequestError } = require("../../../Errors");
 const Users = require("../Models/Users");
 const { purchasedTemplate } = require("../Helpers/mail-templates/plan-purchased");
+const { sendMail } = require("../Helpers/mail-sender");
 const stripe = require("stripe")("sk_test_51OpQGDSCWE6I9nltT5uinyhpTXG5nNh1e6qSNyPpVgorZxaxyOv9YD261Fx6JO9k1qIpjjMA4DKOsvFFmJNted0y007ASDMOEN")
 const checkoutSession = async (userId) => {
   console.log("session",userId);
